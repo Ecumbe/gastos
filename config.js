a2aa320eafce0a88b6281bdd3a9bf0c96c56022c
Ecumbe <eduardo.cumbev@ug.edu.ec>
2026-03-24 15:50:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 // config.js - Configuración y credenciales seguras
 const SUPABASE_URL = 'https://pwvonpwdqynvfrqrbjwy.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3dm9ucHdkcXludmZycXJiand5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzkyOTMsImV4cCI6MjA4OTk1NTI5M30.TCkGbviCkCL3ps6Aex8oah1gE_Ne9mFfI2ovwpneZMw'; 
-// Solución al SyntaxError: Renombramos nuestra variable cliente a "supabaseClient"
-
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
